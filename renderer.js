2cdaa3bc92a2c0c9a16628d427e077d9fdffc70b
hando <enthusapp@gmail.com>
2018-12-21 17:38:22 +0900

    show ipaddress, block drag

--- renderer.js
+++ renderer.js
@@ -135,3 +135,17 @@ function selectRegion(reg) {
   for (const el in regionNodes)
     regionNodes[el].style['color'] = el === reg ? "yellow" : "white";
 }
+
+var ifaces = require('os').networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    document.getElementById('name').innerText = ["Air Display v1.0.0", iface.address].join(' ');
+    return false;
+  });
+});
