b5421f923e2e90e9d453abb6416ddddf378e1087
hando <enthusapp@gmail.com>
2018-12-21 15:13:22 +0900

    use AWS lambda and change UI

--- renderer.js
+++ renderer.js
@@ -1,36 +1,14 @@
 var xhr = new XMLHttpRequest();
-var oParser = new DOMParser();
 
-const DataAPI = 'http://openapi.airkorea.or.kr/openapi/services/rest';
-
-const ServiceAPIs = {
-  listStation: 'MsrstnInfoInqireSvc', // 측정소정보 조회 서비스
-  mesureAir: 'ArpltnInforInqireSvc', // 대기오염정보 조회 서비스
-  statsAir: 'ArpltnStatsSvc', // 대기오염통계 서비스
-  oxizon: 'OzYlwsndOccrrncInforInqireSvc' // 오존황사 발생정보조회 서비스
-};
-
-const APIs = {
-  realTimeEachStation: 'getMsrstnAcctoRltmMesureDnsty', // 측정소별 실시간 측정정보 조회
-  listBadStation: 'getUnityAirEnvrnIdexSnstiveAboveMsrstnList', // 통합대기환경지수 나쁨 이상 측정소 목록조회
-  realTimeEachCity: 'getCtprvnRltmMesureDnsty', // 시도별 실시간 측정정보 조회
-  forecast: 'getMinuDustFrcstDspth', // 미세먼지/오존 예보통보 조회
-  averageEachCity: 'getCtprvnMesureLIst', // 시도별 실시간 평균정보 조회
-  averageEachTown: 'getCtprvnMesureSidoLIst', // 시군구별 실시간 평균정보 조회
-};
-
-const ServiceKey = 'Q9uAszqCfX0MytqG8Lt6h4UPi1LYdYc1EXDveF9k8gHu%2FQecdiFw48KzHnWvB2LndHaUMG3ca3s4ebSqSlUTDg%3D%3D';
-const APIver = '1.3';
-const dataTerm = 'month';
-
-function getRealTimeEachStation() {
-  var sendData = [DataAPI, ServiceAPIs.mesureAir, APIs.realTimeEachStation].join('/');
-  sendData += `?stationName=종로구`;
-  sendData += `&dataTerm=${dataTerm}&pageNo=1&numOfRows=10&ServiceKey=${ServiceKey}&ver=${APIver}`;
-
-  xhr.open('GET', sendData);
-  xhr.send();
+if (process.env.NODE_ENV === 'development') {
+  xhr.open('GET', 'file:///1234');
+} else {
+  xhr.open('GET', 'https://s3.ap-northeast-2.amazonaws.com/enthusair/1234');
 }
+
+var time_result = document.getElementById('time_result');
+var seoul = document.getElementById('seoul_result');
+var jeju = document.getElementById('jeju_result');
 
 function getRealTimeEachCity() {
   switch (state) {
@@ -60,12 +38,10 @@ function getRealTimeEachCity() {
     break;
   }
 
-  var sendData = [DataAPI, ServiceAPIs.mesureAir, APIs.realTimeEachCity].join('/');
-  sendData += '?sidoName=서울';
-  sendData += `&dataTerm=${dataTerm}&pageNo=1&numOfRows=10&ServiceKey=${ServiceKey}&ver=${APIver}`;
+  if (!isFirstArrived)
+    seoul.innerText = '데이터를 읽어오는 중입니다...';
 
-  document.getElementById('result').innerText = '데이터를 읽어오는 중입니다...';
-  xhr.open('GET', sendData);
+  xhr.setRequestHeader("Cache-Control", "max-age=0");
   xhr.send();
 }
 
@@ -106,30 +82,32 @@ function getCoutList(val) {
 xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     if (xhr.status === 200) {
-      data = oParser.parseFromString(xhr.responseText, "text/xml");
-      [].forEach.call(tagSels(data.documentElement, 'item'), el => {
-        if (tagSel(el, 'stationName').textContent === '중구') {
-          console.log(el);
-          var result = `${tagSel(el, 'dataTime').textContent}`;
-          var pm10Grade1h = parseInt(tagSel(el, 'pm10Grade1h').textContent);
-          var pm25Grade1h = parseInt(tagSel(el, 'pm25Grade1h').textContent);
-
-          result += ` 종로구 중구`;
-          result += `,<br>미세: ${tagSel(el, 'pm10Value').textContent} `;
-          result += `μg/㎡(${getCoutList(pm10Grade1h).msg})`;
-          result += `,<br>초미세: ${tagSel(el, 'pm25Value').textContent} `;
-          result += `μg/㎡(${getCoutList(pm25Grade1h).msg})`;
-
-          document.getElementById('result').innerHTML = result;
-          document.getElementById('body').style.backgroundColor = getCoutList(Math.max(pm10Grade1h, pm25Grade1h)).color;
-          isFirstArrived = true;
-        }
-      });
+      var data = JSON.parse(xhr.responseText);
+      time_result.innerText = data['서울'].list[0].dataTime;
+      showResult(seoul, data['서울'].list[0], '서울 중구');
+      showResult(jeju, data['제주'].list[0], '제주 이도동');
+      /*
+      document.getElementById('body').style.backgroundColor =
+        getCoutList(Math.max(pm10Grade1h, pm25Grade1h)).color;*/
+      isFirstArrived = true;
     } else {
       console.log('[' + xhr.status + ']: ' + xhr.statusText);
     }
   }
 };
+
+function showResult(node, data, name) {
+  var result = name;
+  var pm10Grade1h = parseInt(data['pm10Grade1h']);
+  var pm25Grade1h = parseInt(data['pm25Grade1h']);
+
+  result += `, 미세: ${data['pm10Value']} `;
+  result += `μg/㎡(${getCoutList(pm10Grade1h).msg})`;
+  result += `, 초미세: ${data['pm25Value']} `;
+  result += `μg/㎡(${getCoutList(pm25Grade1h).msg})`;
+
+  node.innerHTML = result;
+}
 
 var state = "boot";
 var isFirstArrived = false;
